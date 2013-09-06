fc571b24906d766d5dbacf2b130697fa4fe6baf5
nickniebaum <nickniebaum@gmail.com>
2013-09-06 09:51:25 -0500

    Cookies no longer expire

--- application/config/config.php
+++ application/config/config.php
@@ -245,7 +245,7 @@ $config['encryption_key'] = 'WDDQE3p4Px9f1Pe8573or2WKQuxadvzgWA0DR3af';
 |
 */
 $config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
+$config['sess_expiration']		= 0;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= FALSE;
